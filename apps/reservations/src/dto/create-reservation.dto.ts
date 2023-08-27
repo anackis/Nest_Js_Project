a3828dfdf1e6d5f33952792862fb3059e8eb390d
Alex Anackis <anackis@inbox.lv>
2023-08-27 16:51:38 +0300

    First Commit

---
+++ apps/reservations/src/dto/create-reservation.dto.ts
@@ -0,0 +1,18 @@
+import { IsDate, IsString } from 'class-validator';
+import { Type } from 'class-transformer';
+
+export class CreateReservationDto {
+  @IsDate()
+  @Type(() => Date)
+  startDate: Date;
+
+  @IsDate()
+  @Type(() => Date)
+  endDate: Date;
+
+  @IsString()
+  placeId: string;
+
+  @IsString()
+  invoiceId: string;
+}
